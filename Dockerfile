26d3874b93654d45a00181320604d6ec51a63412
Hahnsang Kim <hkim@inspro9.com>
2020-04-18 21:40:20 -0400

    Added Dockerfile, docker-compose and requirements.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM tiangolo/uvicorn-gunicorn:python3.7-alpine3.8
 
-ENV PYTHONUNBUFFERED=1
 COPY ./requirements.txt /requirements.txt
 RUN pip install -r /requirements.txt
 
--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
-from models.user import User
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/user")
-async def post_user(user: User):
-    return {"request body": user}
-
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
+def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
